45cdf64ae7ea58064a106032e2256b4e8b0c1510
Filipe Medeiros <filipesilvamedeiros@gmail.com>
2018-11-05 14:55:18 +0000

    yes

--- js/clip.js
+++ js/clip.js
@@ -5,8 +5,9 @@ function logIn() {
     const username = document.getElementById('usernameInput').value;
     const password = document.getElementById('passwordInput').value;
 
-    if(username === USERNAME && password === PASSWORD)
+    if(username === USERNAME && password === PASSWORD) {
+        console.log("Logged in");
         window.location.href = "pages/homepage.html";
-    else
+    } else
         alert("Not correct");
 }
